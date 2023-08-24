584261e967d5d6fbad967cb3c0065651c3594bce
Chan-Ho Suh <chanhosuh@gmail.com>
2023-08-24 16:56:11 -0400

    Test non-factory stableswap gauge kill

--- tests/test_kill_gauge.py
+++ tests/test_kill_gauge.py
@@ -1,7 +1,11 @@
 import ape
 import pytest
 
-from curve_dao.addresses import CRYPTOSWAP_FACTORY_OWNER, CURVE_DAO_OWNERSHIP
+from curve_dao.addresses import (
+    CRYPTOSWAP_FACTORY_OWNER,
+    CURVE_DAO_OWNERSHIP,
+    STABLESWAP_GAUGE_OWNER,
+)
 from curve_dao.simulate import simulate
 from curve_dao.vote_utils import make_vote
 
@@ -18,10 +22,16 @@ def tricrypto_ng_gauge():
     yield ape.Contract("0x4fc86cd0f9b650280fa783e3116258e0e0496a2c")
 
 
+@pytest.fixture(scope="module")
+def stableswap_gauge():
+    # USDP metapool (pre-factory)
+    yield ape.Contract("0x055be5ddb7a925bfef3417fc157f53ca77ca7222")
+
+
 def test_kill_factory_gauge(vote_deployer, crypto_factory_gauge):
     assert crypto_factory_gauge.is_killed() is False
 
-    parameter_action = (
+    kill_action = (
         CRYPTOSWAP_FACTORY_OWNER,
         "set_killed",
         crypto_factory_gauge.address,
@@ -30,7 +40,7 @@ def test_kill_factory_gauge(vote_deployer, crypto_factory_gauge):
 
     vote_id = make_vote(
         target=CURVE_DAO_OWNERSHIP,
-        actions=[parameter_action],
+        actions=[kill_action],
         description="test",
         vote_creator=vote_deployer,
     )
@@ -47,7 +57,7 @@ def test_kill_factory_gauge(vote_deployer, crypto_factory_gauge):
 def test_kill_ng_gauge(vote_deployer, tricrypto_ng_gauge):
     assert tricrypto_ng_gauge.is_killed() is False
 
-    parameter_action = (
+    kill_action = (
         tricrypto_ng_gauge.address,  # owner is factory admin
         "set_killed",
         True,
@@ -55,7 +65,7 @@ def test_kill_ng_gauge(vote_deployer, tricrypto_ng_gauge):
 
     vote_id = make_vote(
         target=CURVE_DAO_OWNERSHIP,  # tricrypto-ng factory admin is OWNERSHIP agent
-        actions=[parameter_action],
+        actions=[kill_action],
         description="test",
         vote_creator=vote_deployer,
     )
@@ -67,3 +77,29 @@ def test_kill_ng_gauge(vote_deployer, tricrypto_ng_gauge):
     )
 
     assert tricrypto_ng_gauge.is_killed() is True
+
+
+def test_kill_stableswap_gauge(vote_deployer, stableswap_gauge):
+    assert stableswap_gauge.is_killed() is False
+
+    kill_action = (
+        STABLESWAP_GAUGE_OWNER,
+        "set_killed",
+        stableswap_gauge.address,
+        True,
+    )
+
+    vote_id = make_vote(
+        target=CURVE_DAO_OWNERSHIP,
+        actions=[kill_action],
+        description="test",
+        vote_creator=vote_deployer,
+    )
+
+    # this advances the chain one week from vote creation
+    simulate(
+        vote_id=vote_id,
+        voting_contract=CURVE_DAO_OWNERSHIP["voting"],
+    )
+
+    assert stableswap_gauge.is_killed() is True
